f8c8231fea731d56548e11d204fb09fa6253da07
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-26 10:47:05 -0600

    Created IntakePivotIOSparkMax

---
+++ src/main/java/com/team6647/subsystems/intake/IntakePivotIOSparkMax.java
@@ -0,0 +1,46 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ * 
+ * 26 01 2024
+ */
+
+package com.team6647.subsystems.intake;
+
+import com.andromedalib.motorControllers.SuperSparkMax;
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.SparkAbsoluteEncoder.Type;
+import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.team6647.util.Constants.IntakeConstants;
+
+public class IntakePivotIOSparkMax implements IntakePivotIO {
+    private static SuperSparkMax leftIntakePivotMotor = new SuperSparkMax(
+            IntakeConstants.intakePivotLeftMotorID,
+            GlobalIdleMode.Coast, IntakeConstants.intakePivotLeftMotorInverted,
+            IntakeConstants.intakeMotorsCurrentLimit,
+            IntakeConstants.intakePivotEncoderPositionConversionFactor,
+            IntakeConstants.intakePivotEncoderZeroOffset, IntakeConstants.intakePivotEncoderInverted);
+    private static SuperSparkMax rightIntakePivotMotor = new SuperSparkMax(
+            IntakeConstants.intakePivotRightMotorID,
+            GlobalIdleMode.Coast, IntakeConstants.intakePivotRightMotorInverted,
+            IntakeConstants.intakeMotorsCurrentLimit);
+
+    private static AbsoluteEncoder pivotEncoder;
+
+    public IntakePivotIOSparkMax() {
+        pivotEncoder = leftIntakePivotMotor.getAbsoluteEncoder(Type.kDutyCycle);
+    }
+
+    @Override
+    public void updateInputs(IntakePivoIOInputs inputs) {
+        inputs.intakePivotMotorVelocity = leftIntakePivotMotor.getVelocity();
+        inputs.intakePivotMotorAppliedVoltage = leftIntakePivotMotor.getAppliedOutput();
+        inputs.intakePivotMotorPosition = leftIntakePivotMotor.getPosition();
+        inputs.intakePivotAbsoluteEncoderPosition = pivotEncoder.getPosition();
+    }
+
+    @Override
+    public void setIntakeVoltage(double voltage) {
+        leftIntakePivotMotor.setVoltage(voltage);
+        rightIntakePivotMotor.setVoltage(voltage);
+    }
+}
