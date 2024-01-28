251e45e5ed156fe615081f20b0fb27c495ad5958
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-28 10:15:35 -0600

    Filtered vision measurement implementation

--- src/main/java/com/team6647/subsystems/vision/VisionAutoSubsystem.java
+++ src/main/java/com/team6647/subsystems/vision/VisionAutoSubsystem.java
@@ -8,18 +8,7 @@ package com.team6647.subsystems.vision;
 import org.littletonrobotics.junction.Logger;
 
 import com.andromedalib.andromedaSwerve.subsystems.AndromedaSwerve;
-import com.andromedalib.andromedaSwerve.utils.LocalADStarAK;
-import com.pathplanner.lib.auto.AutoBuilder;
-import com.pathplanner.lib.path.PathConstraints;
-import com.pathplanner.lib.pathfinding.Pathfinding;
-import com.pathplanner.lib.util.PathPlannerLogging;
-import com.team6647.util.Constants.DriveConstants;
 
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class VisionAutoSubsystem extends SubsystemBase {
@@ -55,7 +44,9 @@ public class VisionAutoSubsystem extends SubsystemBase {
 
   public void computeVisionMeasurements() {
     if (inputs.hasTarget) {
-      andromedaSwerve.addVisionMeasurements(inputs.observedPose2d, inputs.timestampLatency);
+      if (inputs.targetDistance < 1.5) {
+        andromedaSwerve.addVisionMeasurements(inputs.observedPose2d, inputs.timestampLatency);
+      }
     }
   }
 }
--- src/main/java/com/team6647/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/com/team6647/subsystems/vision/VisionIOLimelight.java
@@ -9,7 +9,9 @@ import org.littletonrobotics.junction.Logger;
 import com.andromedalib.vision.LimelightHelpers;
 
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 
 public class VisionIOLimelight implements VisionIO {
@@ -22,19 +24,42 @@ public class VisionIOLimelight implements VisionIO {
 
     @Override
     public synchronized void updateInputs(VisionIOInputs inputs) {
+
+        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+        NetworkTableEntry ty = table.getEntry("ty");
+        double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+        // how many degrees back is your limelight rotated from perfectly vertical?
+        double limelightMountAngleDegrees = -30;
+
+        // distance from the center of the Limelight lens to the floor
+        double limelightLensHeightMeters = 0.58;
+
+        // distance from the target to the floor
+        double goalHeightMeters = 0.142;
+
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+        // calculate distance
+        double distanceFromLimelightToGoalMeters = (goalHeightMeters - limelightLensHeightMeters)
+                / Math.tan(angleToGoalRadians);
+
+        inputs.targetDistance = distanceFromLimelightToGoalMeters;
+
         LimelightHelpers.Results result = LimelightHelpers.getLatestResults("limelight").targetingResults;
 
         inputs.TA = LimelightHelpers.getTA("limelight");
 
         if (!(result.botpose[0] == 0 && result.botpose[1] == 0) &&
-                LimelightHelpers.getTA("limelight") < 30) {
+                LimelightHelpers.getTA("limelight") > 0.1) {
             inputs.hasTarget = true;
 
             inputs.observedPose2d = alliance == Alliance.Blue ? LimelightHelpers.toPose2D(result.botpose_wpiblue)
                     : LimelightHelpers.toPose2D(result.botpose_wpired);
 
-            inputs.timestampLatency = Logger.getRealTimestamp() - (result.latency_capture / 1000.0) -
-                    (result.latency_pipeline / 1000.0);
+            inputs.timestampLatency = Logger.getRealTimestamp()
+                    - (result.latency_capture + result.latency_pipeline / 1000.0);
 
         } else {
             inputs.observedPose2d = new Pose2d();
