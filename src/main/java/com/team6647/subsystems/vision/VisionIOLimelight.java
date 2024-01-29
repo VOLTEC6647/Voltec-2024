d6cc7f5dc5281479be5a4e0e38120d146c2bc7e1
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-29 09:02:24 -0600

    Cleaned distance computation

--- src/main/java/com/team6647/subsystems/vision/VisionIOLimelight.java
+++ src/main/java/com/team6647/subsystems/vision/VisionIOLimelight.java
@@ -18,6 +18,14 @@ public class VisionIOLimelight implements VisionIO {
 
     private Alliance alliance;
 
+    // how many degrees back is your limelight rotated from perfectly vertical?
+    private double limelightMountAngleDegrees = -30;
+
+    // distance from the center of the Limelight lens to the floor
+    private double limelightLensHeightMeters = 0.6;
+
+    private double goalHeightMeters = 0.142;
+
     public VisionIOLimelight(Alliance alliance) {
         this.alliance = alliance;
     }
@@ -25,27 +33,7 @@ public class VisionIOLimelight implements VisionIO {
     @Override
     public synchronized void updateInputs(VisionIOInputs inputs) {
 
-        NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-        NetworkTableEntry ty = table.getEntry("ty");
-        double targetOffsetAngle_Vertical = ty.getDouble(0.0);
-
-        // how many degrees back is your limelight rotated from perfectly vertical?
-        double limelightMountAngleDegrees = -30;
-
-        // distance from the center of the Limelight lens to the floor
-        double limelightLensHeightMeters = 0.58;
-
-        // distance from the target to the floor
-        double goalHeightMeters = 0.142;
-
-        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
-
-        // calculate distance
-        double distanceFromLimelightToGoalMeters = (goalHeightMeters - limelightLensHeightMeters)
-                / Math.tan(angleToGoalRadians);
-
-        inputs.targetDistance = distanceFromLimelightToGoalMeters;
+        inputs.targetDistance = computeDistance();
 
         LimelightHelpers.Results result = LimelightHelpers.getLatestResults("limelight").targetingResults;
 
@@ -66,6 +54,17 @@ public class VisionIOLimelight implements VisionIO {
             inputs.timestampLatency = 0;
             inputs.hasTarget = false;
         }
+    }
+
+    public double computeDistance() {
+        double targetOffsetVertical = LimelightHelpers.getTY("limelight");
+
+        double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetVertical;
+        double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+        // calculate distance
+        return (goalHeightMeters - limelightLensHeightMeters)
+                / Math.tan(angleToGoalRadians);
     }
 
 }
