57d7818d31842324d5587eb90685ddedd8dd6d1e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-01-29 09:26:21 -0600

    Finished ElevatorIOSparkMax

--- src/main/java/com/team6647/subsystems/elevator/ElevatorIOSparkMax.java
+++ src/main/java/com/team6647/subsystems/elevator/ElevatorIOSparkMax.java
@@ -6,5 +6,32 @@
 
 package com.team6647.subsystems.elevator;
 
+import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.andromedalib.motorControllers.SuperSparkMax;
+import com.team6647.util.Constants.ElevatorConstants;
+
 public class ElevatorIOSparkMax implements ElevatorIO {
+    private static SuperSparkMax elevatorMotor = new SuperSparkMax(
+            ElevatorConstants.elevatorMotorID,
+            GlobalIdleMode.brake,
+            ElevatorConstants.elevatorMotorInverted,
+            ElevatorConstants.elevatorMotorCurrentLimit,
+            ElevatorConstants.elevatorAbsoluteEncoderPositionConversionFactor,
+            ElevatorConstants.elevatorAbsoluteEncoderZeroOffset,
+            ElevatorConstants.elevatorAbsoluteEncoderInverted);
+
+    @Override
+    public void updateInputs(ElevatorIOInputs inputs) {
+        inputs.elevatorPosition = elevatorMotor.getPosition();
+        inputs.elevatorAbsoluteEncoderPosition = 0.0; // TODO SET
+
+        inputs.topBeambrake = false; // TODO SET
+        inputs.bottomBeamBrake = false; // TODO SET
+        inputs.elevatorAppliedVolts = elevatorMotor.getBusVoltage();
+    }
+
+    @Override
+    public void setElevatorVoltage(double volts) {
+        elevatorMotor.setVoltage(volts);
+    }
 }
