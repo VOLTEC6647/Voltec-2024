f2a5703612189bcf2495f32d2691d8821199d257
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-02 08:12:11 -0600

    Added change pipeline

--- src/main/java/com/team6647/subsystems/vision/VisionIO.java
+++ src/main/java/com/team6647/subsystems/vision/VisionIO.java
@@ -14,13 +14,21 @@ public interface VisionIO {
     @AutoLog
     public static class VisionIOInputs {
         public double TA = 0.0;
+        public double TY = 0.0;
+        public double TX = 0.0;
+        public int currentPipelineNumber = 0;
         public Pose2d observedPose2d = new Pose2d();
         public double timestampLatency = 0.0;
         public boolean hasTarget = false;
         public double targetDistance = 0.0;
+        public int targetID = 0;
     }
 
     /** Updates the set of loggable inputs. */
     public default void updateInputs(VisionIOInputs inputs) {
+    }
+
+    public default void changePipeline(int pipelineNumber) {
+
     }
 }
