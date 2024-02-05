4b94605f8447ec71606bfce18c6375fd89be85fd
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-05 10:30:00 -0600

    Added GeomUtil

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 63;
-  public static final String GIT_SHA = "21a4f6863326235f8b32963fb38fb86ba8e78b64";
-  public static final String GIT_DATE = "2024-01-27 17:59:24 GMT-06:00";
+  public static final int GIT_REVISION = 82;
+  public static final String GIT_SHA = "5ccac61cbc88d625dca3f0e58753255a46837cc4";
+  public static final String GIT_DATE = "2024-01-30 18:39:33 GMT-06:00";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-27 21:38:42 GMT-06:00";
-  public static final long BUILD_UNIX_TIME = 1706413122423L;
+  public static final String BUILD_DATE = "2024-02-06 19:57:56 GMT-06:00";
+  public static final long BUILD_UNIX_TIME = 1707271076497L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
