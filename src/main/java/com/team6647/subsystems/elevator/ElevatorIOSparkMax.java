d0f454a64ef0d282126bd7b9479d35f2a76969a6
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-09 23:31:41 -0600

    Added reset to absolute for elevator

--- src/main/java/com/team6647/subsystems/elevator/ElevatorIOSparkMax.java
+++ src/main/java/com/team6647/subsystems/elevator/ElevatorIOSparkMax.java
@@ -7,38 +7,40 @@
 package com.team6647.subsystems.elevator;
 
 import com.andromedalib.motorControllers.IdleManager.GlobalIdleMode;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
 import com.andromedalib.motorControllers.SuperSparkMax;
 import com.team6647.util.Constants.ElevatorConstants;
-
-import edu.wpi.first.math.geometry.Rotation2d;
 
 public class ElevatorIOSparkMax implements ElevatorIO {
     private static SuperSparkMax elevatorTopMotor = new SuperSparkMax(
             ElevatorConstants.elevatorTopMotorID,
-            GlobalIdleMode.brake,
+            GlobalIdleMode.Coast,
             ElevatorConstants.elevatorTopMotorInverted,
             ElevatorConstants.elevatorMotorCurrentLimit);
 
     private static SuperSparkMax elevatorBottomMotor = new SuperSparkMax(
             ElevatorConstants.elevatorBoottomMotorID,
-            GlobalIdleMode.brake,
+            GlobalIdleMode.Coast,
             ElevatorConstants.elevatorTopMotorInverted,
             ElevatorConstants.elevatorMotorCurrentLimit);
 
     private static CANcoder elevatorCANCoder = new CANcoder(ElevatorConstants.elevatorCANCoderID);
 
-    private static Rotation2d elevatorOffset = new Rotation2d(ElevatorConstants.elevatorCANCoderOffset);
-
     public ElevatorIOSparkMax() {
+        configCANCoder();
 
+        elevatorTopMotor.setPositionConversionFactor(ElevatorConstants.elevatorGearRatio);
+        elevatorBottomMotor.setPositionConversionFactor(ElevatorConstants.elevatorGearRatio);
+
+        setElevatorPosition();
     }
 
     @Override
     public void updateInputs(ElevatorIOInputs inputs) {
         inputs.elevatorPosition = elevatorTopMotor.getPosition();
-        inputs.elevatorAbsoluteEncoderPosition = elevatorCANCoder.getAbsolutePosition().getValueAsDouble()
-                - elevatorOffset.getRotations();
+        inputs.elevatorAbsoluteEncoderPosition = elevatorCANCoder.getAbsolutePosition().getValueAsDouble();
 
         inputs.elevatorAppliedVolts = elevatorTopMotor.getBusVoltage();
     }
@@ -47,5 +49,20 @@ public class ElevatorIOSparkMax implements ElevatorIO {
     public void setElevatorVoltage(double volts) {
         elevatorTopMotor.setVoltage(volts);
         elevatorBottomMotor.setVoltage(volts);
+    }
+
+    public void configCANCoder() {
+        elevatorCANCoder.getConfigurator().apply(new CANcoderConfiguration());
+        CANcoderConfiguration config = new CANcoderConfiguration();
+        config.MagnetSensor.SensorDirection = ElevatorConstants.elevatorCANCoderSensorValue;
+        config.MagnetSensor.MagnetOffset = ElevatorConstants.elevatorCANCoderOffset;
+        config.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Unsigned_0To1;
+        elevatorCANCoder.getConfigurator().apply(config);
+    }
+
+    public void setElevatorPosition() {
+        double absPos = elevatorCANCoder.getAbsolutePosition().getValueAsDouble();
+
+        elevatorTopMotor.setPosition(absPos);
     }
 }
