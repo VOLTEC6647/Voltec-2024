a9173ac248d1ab05fea4dff5863cd22b06e9b031
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-11 15:15:12 -0600

    Finished ShooterPivotSubsystem

--- src/main/java/com/team6647/subsystems/shooter/ShooterPivotSubsystem.java
+++ src/main/java/com/team6647/subsystems/shooter/ShooterPivotSubsystem.java
@@ -6,23 +6,35 @@
 
 package com.team6647.subsystems.shooter;
 
+import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
+import com.andromedalib.math.Functions;
 import com.team6647.util.Constants.ShooterConstants;
 
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterPivotSubsystem extends SubsystemBase {
 
   private static ShooterPivotSubsystem instance;
 
-  private static ShooterPivotState mState;
+  private static ShooterPivotState mState = ShooterPivotState.HOMED;
 
   private ShooterPivotIO io;
   private ShooterPivotIOInputsAutoLogged inputs = new ShooterPivotIOInputsAutoLogged();
 
+  private ProfiledPIDController mPivotController = new ProfiledPIDController(ShooterConstants.pivotKp,
+      ShooterConstants.piovtKi, ShooterConstants.piovtKi,
+      new TrapezoidProfile.Constraints(ShooterConstants.pivotMaxVelocity, ShooterConstants.pivotMaxAcceleration));
+
+  @AutoLogOutput(key = "Shooter/Pivot/Setpoint")
+  private double setpoint = ShooterConstants.pivotHomedPosition;
+
   /** Creates a new ShooterPivotSubsystem. */
-  private ShooterPivotSubsystem(ShooterPivotIO io) {
+  private ShooterPivotSubsystem(
+      ShooterPivotIO io) {
     this.io = io;
   }
 
@@ -41,22 +53,29 @@ public class ShooterPivotSubsystem extends SubsystemBase {
     if (mState == ShooterPivotState.SHOOTING) {
       calculateShooterAngle();
     }
+
+    computePID();
+
   }
 
   public enum ShooterPivotState {
     HOMED,
-    SHOOTING
+    SHOOTING,
+    INDEXING,
   }
 
   public void setShooterPivotState(ShooterPivotState state) {
     switch (state) {
       case HOMED:
         mState = ShooterPivotState.HOMED;
-        setShooterSetpoint(ShooterConstants.shooterHomedPosition);
+        changeSetpoint(ShooterConstants.pivotHomedPosition);
         break;
       case SHOOTING:
         mState = ShooterPivotState.SHOOTING;
         break;
+      case INDEXING:
+        mState = ShooterPivotState.INDEXING;
+        changeSetpoint(ShooterConstants.pivotIndexingPosition);
       default:
         break;
     }
@@ -66,7 +85,23 @@ public class ShooterPivotSubsystem extends SubsystemBase {
 
   }
 
-  public void setShooterSetpoint(double setpoint) {
+  private void changeSetpoint(double newSetpoint) {
+    if (newSetpoint > ShooterConstants.pivotMaxPosition || newSetpoint < ShooterConstants.pivotMinPosition) {
+      newSetpoint = Functions.clamp(newSetpoint, ShooterConstants.pivotMinPosition,
+          ShooterConstants.pivotMaxPosition);
+    }
 
+    setpoint = newSetpoint;
+  }
+
+  public void computePID() {
+    double output = mPivotController.calculate(inputs.shooterAbsoluteEncoderPosition, setpoint);
+
+    output = Functions.clamp(output, 0.2, 0.2);
+
+    Logger.recordOutput("Shooter/Pivot/output", output);
+    output = output * 12;
+
+    io.setShooterVoltage(output);
   }
 }
