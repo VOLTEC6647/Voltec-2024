82b5c97b6407c439b5f68ef105bcc007b421bf42
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-15 15:48:43 -0600

    Added Field constants

--- src/main/java/com/team6647/util/Constants.java
+++ src/main/java/com/team6647/util/Constants.java
@@ -11,6 +11,8 @@ import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.interpolation.InterpolatingDoubleTreeMap;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -47,6 +49,8 @@ public class Constants {
         public static class RobotConstants {
                 public static final Mode currentMode = Mode.REAL;
 
+                public static final boolean tuningMode = true;
+
                 public static Mode getMode() {
                         if ((currentMode == Mode.SIM || currentMode == Mode.REPLAY) && RobotBase.isReal()) {
                                 DriverStation.reportError("[Mode Error] Mode is set to " + currentMode.toString()
@@ -69,6 +73,87 @@ public class Constants {
                 }
         }
 
+        /**
+         * Contains various field dimensions and useful reference points. Dimensions are
+         * in meters, and sets
+         * of corners start in the lower left moving clockwise. <b>All units in
+         * Meters</b> <br>
+         * <br>
+         *
+         * <p>
+         * All translations and poses are stored with the origin at the rightmost point
+         * on the BLUE
+         * ALLIANCE wall.<br>
+         * <br>
+         * Length refers to the <i>x</i> direction (as described by wpilib) <br>
+         * Width refers to the <i>y</i> direction (as described by wpilib)
+         */
+        public static class FieldConstants {
+                public static double fieldLength = Units.inchesToMeters(651.223);
+                public static double fieldWidth = Units.inchesToMeters(323.277);
+                public static double wingX = Units.inchesToMeters(229.201);
+                public static double podiumX = Units.inchesToMeters(126.75);
+                public static double startingLineX = Units.inchesToMeters(74.111);
+
+                public static Translation2d ampCenter = new Translation2d(Units.inchesToMeters(72.455),
+                                Units.inchesToMeters(322.996));
+
+                /** Staging locations for each note */
+                public static final class StagingLocations {
+                        public static double centerlineX = fieldLength / 2.0;
+
+                        // need to update
+                        public static double centerlineFirstY = Units.inchesToMeters(29.638);
+                        public static double centerlineSeparationY = Units.inchesToMeters(66);
+                        public static double spikeX = Units.inchesToMeters(114);
+                        // need
+                        public static double spikeFirstY = Units.inchesToMeters(161.638);
+                        public static double spikeSeparationY = Units.inchesToMeters(57);
+
+                        public static Translation2d[] centerlineTranslations = new Translation2d[5];
+                        public static Translation2d[] spikeTranslations = new Translation2d[3];
+
+                        static {
+                                for (int i = 0; i < centerlineTranslations.length; i++) {
+                                        centerlineTranslations[i] = new Translation2d(centerlineX,
+                                                        centerlineFirstY + (i * centerlineSeparationY));
+                                }
+                        }
+
+                        static {
+                                for (int i = 0; i < spikeTranslations.length; i++) {
+                                        spikeTranslations[i] = new Translation2d(spikeX,
+                                                        spikeFirstY + (i * spikeSeparationY));
+                                }
+                        }
+                }
+
+                /** Each corner of the speaker * */
+                public static final class Speaker {
+
+                        // corners (blue alliance origin)
+                        public static Translation3d topRightSpeaker = new Translation3d(
+                                        Units.inchesToMeters(18.055),
+                                        Units.inchesToMeters(238.815),
+                                        Units.inchesToMeters(83.091));
+
+                        public static Translation3d topLeftSpeaker = new Translation3d(
+                                        Units.inchesToMeters(18.055),
+                                        Units.inchesToMeters(197.765),
+                                        Units.inchesToMeters(83.091));
+
+                        public static Translation3d bottomRightSpeaker = new Translation3d(0.0,
+                                        Units.inchesToMeters(238.815), Units.inchesToMeters(78.324));
+                        public static Translation3d bottomLeftSpeaker = new Translation3d(0.0,
+                                        Units.inchesToMeters(197.765), Units.inchesToMeters(78.324));
+
+                        /** Center of the speaker opening (blue alliance) */
+                        public static Translation3d centerSpeakerOpening = bottomLeftSpeaker
+                                        .interpolate(topRightSpeaker, 0.5);
+                }
+
+        }
+
         public static class ShooterConstants {
                 public static final int shooterPivotMotorID = 20;
                 public static final int flywheelBottomMotorID = 21;
@@ -82,12 +167,14 @@ public class Constants {
 
                 public static final double shooterTolerance = 0.0;
 
-                public static final double pivotKp = 0.028;
-                public static final double piovtKi = 0.000029;
-                public static final double pivotKd = 0.0;
+                public static final double pivotKp = 0.19;
+                public static final double pivotKi = 0.0050;
+                public static final double pivotKd = 0.00002;
 
-                public static final double pivotMaxVelocity = 10.0;
-                public static final double pivotMaxAcceleration = 10.0;
+                public static final double pivotParallelFloorOffset = 203.0478515625;
+
+                public static final double pivotMaxVelocity = 1500.0;
+                public static final double pivotMaxAcceleration = 1500.0;
 
                 public static final int shooterBeamBrakeChannel = 8;
                 public static final int shooterMotorCurrentLimit = 80;
@@ -98,14 +185,26 @@ public class Constants {
                 public static final boolean armEncoderInverted = true;
                 public static final boolean shooterPivotMotorInverted = true;
 
-                public static final double pivotMinPosition = 113;
+                public static final double pivotMinPosition = 114;
                 public static final double pivotMaxPosition = 257.750;
-                public static final double pivotHomedPosition = 114;
-                public static final double pivotIndexingPosition = 150;
+                public static final double pivotHomedPosition = 115;
+                public static final double pivotIndexingPosition = 152;
 
                 public static final double rollerIntakingVelocity = 0.25;
                 public static final double rollerExhaustingVelocity = -0.25;
                 public static final double rollerIdleVelocity = 0.1;
+
+                public static final InterpolatingDoubleTreeMap shooterRPMMap = new InterpolatingDoubleTreeMap();
+
+                static {
+                        shooterRPMMap.put(0., 0.);
+                }
+
+                public static final InterpolatingDoubleTreeMap shooterPivotMap = new InterpolatingDoubleTreeMap();
+
+                static {
+                        shooterPivotMap.put(0., 0.);
+                }
         }
 
         public static class ElevatorConstants {
@@ -114,7 +213,7 @@ public class Constants {
 
                 public static final int elevatorCANCoderID = 19;
 
-                public static final double elevatorCANCoderOffset = 0.0;
+                public static final double elevatorCANCoderOffset = -0.940000;
 
                 public static final boolean elevatorTopMotorInverted = false;
                 public static final boolean elevatorBottomMotorInverted = true;
@@ -127,6 +226,7 @@ public class Constants {
 
                 public static final double elevatorMinPosition = 0.64;
                 public static final double elevatorMaxPosition = 0.8;
+                public static final double elevatorHomedPosition = 0.1;
                 public static final double elevatorShootingPosition = 0.70;
 
                 public static final SensorDirectionValue elevatorCANCoderSensorValue = SensorDirectionValue.CounterClockwise_Positive;
@@ -153,7 +253,7 @@ public class Constants {
                 public static final double maxIntakePivotPosition = 197.040;
                 public static final double intakePivotPositionTolerance = 0.4;
                 public static final double intakeHomedPosition = 136.342;
-                public static final double intakeExtendedPosition = 197.000;
+                public static final double intakeExtendedPosition = 195.000;
 
                 public static final int intakePivotLeftMotorID = 14;
                 public static final int intakePivotRightMotorID = 15;
@@ -173,7 +273,6 @@ public class Constants {
                 public static final double intakeIdleVelocity = 0.1;
 
                 public static final int intakeBeamBrakeChannel = 9;
-
         }
 
         public static class DriveConstants {
