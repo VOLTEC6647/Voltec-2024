8d5d2f53c61b2ea473c4f7331514e10e4cc09b1b
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-15 22:03:28 -0600

    Created Driver1 pathfinding commands

--- src/main/java/com/team6647/subsystems/SuperStructure.java
+++ src/main/java/com/team6647/subsystems/SuperStructure.java
@@ -9,18 +9,28 @@ package com.team6647.subsystems;
 
 import org.littletonrobotics.junction.AutoLogOutput;
 
+import com.andromedalib.andromedaSwerve.subsystems.AndromedaSwerve;
+import com.andromedalib.math.GeomUtil;
+import com.team6647.RobotContainer;
 import com.team6647.subsystems.intake.IntakeCommands;
 import com.team6647.subsystems.intake.IntakePivotSubsystem.IntakePivotState;
 import com.team6647.subsystems.shooter.ShooterCommands;
 import com.team6647.subsystems.shooter.ShooterPivotSubsystem.ShooterPivotState;
+import com.team6647.util.AllianceFlipUtil;
+import com.team6647.util.ShootingCalculatorUtil;
+import com.team6647.util.Constants.FieldConstants;
 import com.team6647.util.Constants.RobotConstants.RollerState;
+import com.team6647.util.ShootingCalculatorUtil.ShootingParameters;
 
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
 public class SuperStructure {
 
     private static SuperStructure instance;
+
+    private AndromedaSwerve andromedaSwerve = RobotContainer.andromedaSwerve;
 
     @AutoLogOutput(key = "SuperStructure/State")
     private SuperStructureState mRobotState = SuperStructureState.IDLE;
@@ -73,5 +83,23 @@ public class SuperStructure {
         return Commands.parallel(
                 IntakeCommands.getTargetPivotStateCommand(IntakePivotState.HOMED),
                 ShooterCommands.getTargetShooterPivotCommand(ShooterPivotState.HOMED));
+    }
+
+    public Command shootingWhileMoving() {
+        mRobotState = SuperStructureState.SHOOTING_MOVING;
+
+        ShootingParameters x = ShootingCalculatorUtil.calculateShootingParameters(andromedaSwerve.getPose(),
+                andromedaSwerve.getFieldRelativeChassisSpeeds(), andromedaSwerve.getSwerveAngle());
+
+        return Commands.waitSeconds(0.1);
+    }
+
+    public Command goToAmp() {
+        return andromedaSwerve.getPathFindPath(GeomUtil.toPose2d(AllianceFlipUtil.apply(FieldConstants.ampCenter)));
+    }
+
+    public Command goToSpeaker() {
+        return andromedaSwerve.getPathFindPath(GeomUtil
+                .toPose2d(AllianceFlipUtil.apply(FieldConstants.Speaker.centerSpeakerOpening.toTranslation2d())));
     }
 }
