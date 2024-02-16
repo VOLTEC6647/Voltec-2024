d5c80d944e7c6a74c8c5544b6d836079f54b0937
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-16 09:57:03 -0600

    Added FlywheelCommand

--- src/main/java/com/team6647/subsystems/shooter/ShooterCommands.java
+++ src/main/java/com/team6647/subsystems/shooter/ShooterCommands.java
@@ -8,6 +8,7 @@ package com.team6647.subsystems.shooter;
 
 import com.team6647.RobotContainer;
 import com.team6647.subsystems.shooter.ShooterPivotSubsystem.ShooterPivotState;
+import com.team6647.subsystems.shooter.ShooterSubsystem.FlywheelState;
 import com.team6647.util.Constants.RobotConstants.RollerState;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -17,6 +18,7 @@ import edu.wpi.first.wpilibj2.command.StartEndCommand;
 public class ShooterCommands {
     private static ShooterSubsystem shooterSubsystem = RobotContainer.shooterSubsystem;
     private static ShooterPivotSubsystem pivotSubsystem = RobotContainer.shooterPivotSubsystem;
+    private static ShooterRollerSubsystem rollerSubsystem = RobotContainer.shooterRollerSubsystem;
 
     public static final Command getTargetShooterPivotCommand(ShooterPivotState desiredState) {
         return new FunctionalCommand(
@@ -29,8 +31,14 @@ public class ShooterCommands {
     }
 
     public static final Command getTargetRollersCommand(RollerState desiredState) {
-        return new StartEndCommand(() -> shooterSubsystem.changeRollerState(desiredState),
-                () -> shooterSubsystem.changeRollerState(RollerState.STOPPED),
+        return new StartEndCommand(() -> rollerSubsystem.changeRollerState(desiredState),
+                () -> rollerSubsystem.changeRollerState(RollerState.STOPPED),
                 shooterSubsystem);
     }
+
+    public static final Command getTargetFlywheelCommand(FlywheelState deisredState) {
+        return new StartEndCommand(() -> shooterSubsystem.changeFlywheelState(deisredState),
+                () -> shooterSubsystem.changeFlywheelState(FlywheelState.IDLE), shooterSubsystem);
+    }
+
 }
