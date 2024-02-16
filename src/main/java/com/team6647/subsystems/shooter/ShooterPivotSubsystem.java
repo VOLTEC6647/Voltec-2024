b5950c1795982f711dba4dc1c8775be5b9d88806
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-15 22:02:13 -0600

    Removed pivot characterization

--- src/main/java/com/team6647/subsystems/shooter/ShooterPivotSubsystem.java
+++ src/main/java/com/team6647/subsystems/shooter/ShooterPivotSubsystem.java
@@ -6,10 +6,6 @@
 
 package com.team6647.subsystems.shooter;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.DegreesPerSecond;
-import static edu.wpi.first.units.Units.Volts;
-
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
@@ -17,65 +13,30 @@ import com.andromedalib.math.Functions;
 import com.team6647.util.LoggedTunableNumber;
 import com.team6647.util.Constants.ShooterConstants;
 
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-import edu.wpi.first.units.Angle;
-import edu.wpi.first.units.Measure;
-import edu.wpi.first.units.MutableMeasure;
-import edu.wpi.first.units.Velocity;
-import edu.wpi.first.units.Voltage;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class ShooterPivotSubsystem extends SubsystemBase {
 
   private static ShooterPivotSubsystem instance;
 
   @AutoLogOutput(key = "Shooter/Pivot/State")
-  private static ShooterPivotState mState = ShooterPivotState.HOMED;
+  private static ShooterPivotState mState = ShooterPivotState.INDEXING;
 
   private ShooterPivotIO io;
   private ShooterPivotIOInputsAutoLogged inputs = new ShooterPivotIOInputsAutoLogged();
 
-  private ProfiledPIDController mPivotController = new ProfiledPIDController(ShooterConstants.pivotKp,
-      ShooterConstants.pivotKi, ShooterConstants.pivotKd,
-      new TrapezoidProfile.Constraints(ShooterConstants.pivotMaxVelocity, ShooterConstants.pivotMaxAcceleration));
-
   @AutoLogOutput(key = "Shooter/Pivot/Setpoint")
-  private double setpoint = ShooterConstants.pivotHomedPosition;
+  private double setpoint = ShooterConstants.pivotIndexingPosition;
 
-  private final MutableMeasure<Voltage> m_appliedVoltage = MutableMeasure.mutable(Volts.of(0));
-  private final MutableMeasure<Angle> m_angle = MutableMeasure.mutable(Degrees.of(0));
-  private final MutableMeasure<Velocity<Angle>> m_velocity = MutableMeasure.mutable(DegreesPerSecond.of(0));
-
-  private final SysIdRoutine m_sysIdRoutine = new SysIdRoutine(
-      new SysIdRoutine.Config(),
-      new SysIdRoutine.Mechanism(
-          (Measure<Voltage> volts) -> {
-            runPivotCharacterization(volts);
-          },
-          log -> {
-            log.motor("pivot")
-                .voltage(
-                    m_appliedVoltage.mut_replace(
-                        inputs.shooterPivotAppliedVolts, Volts))
-                .angularPosition(m_angle.mut_replace(inputs.shooterAbsoluteEncoderPosition, Degrees))
-                .angularVelocity(m_velocity.mut_replace(inputs.shooterAbsoluteEncoderVelocity, DegreesPerSecond));
-          },
-          this));
-
-  private LoggedTunableNumber pivotKp = new LoggedTunableNumber("Shooter/Pivot/kp", ShooterConstants.revPivotKp);
-  private LoggedTunableNumber pivotKi = new LoggedTunableNumber("Shooter/Pivot/ki", ShooterConstants.revPivotKi);
-  private LoggedTunableNumber pivotKd = new LoggedTunableNumber("Shooter/Pivot/kd", ShooterConstants.revPivotKd);
-  private LoggedTunableNumber pivotKf = new LoggedTunableNumber("Shooter/Pivot/kf", ShooterConstants.revPivotKf);
+  private LoggedTunableNumber pivotKp = new LoggedTunableNumber("Shooter/Pivot/kp", ShooterConstants.pivotKp);
+  private LoggedTunableNumber pivotKi = new LoggedTunableNumber("Shooter/Pivot/ki", ShooterConstants.pivotKi);
+  private LoggedTunableNumber pivotKd = new LoggedTunableNumber("Shooter/Pivot/kd", ShooterConstants.pivotKd);
+  private LoggedTunableNumber pivotKf = new LoggedTunableNumber("Shooter/Pivot/kf", ShooterConstants.pivotKf);
 
   /** Creates a new ShooterPivotSubsystem. */
   private ShooterPivotSubsystem(
       ShooterPivotIO io) {
     this.io = io;
-
-    mPivotController.reset(setpoint);
   }
 
   public static ShooterPivotSubsystem getInstance(ShooterPivotIO io) {
@@ -91,7 +52,7 @@ public class ShooterPivotSubsystem extends SubsystemBase {
     Logger.processInputs("Shooter/Pivot", inputs);
 
     LoggedTunableNumber.ifChanged(hashCode(), pid -> {
-      io.setPID(pid[0], pid[1], pid[2], pid[3]);
+      io.setPIDF(pid[0], pid[1], pid[2], pid[3]);
     }, pivotKp, pivotKi, pivotKd, pivotKf);
   }
 
@@ -133,18 +94,7 @@ public class ShooterPivotSubsystem extends SubsystemBase {
   }
 
   public boolean inTolerance() {
-    return mPivotController.atGoal();
+    return inputs.inTolerance;
   }
 
-  public void runPivotCharacterization(Measure<Voltage> volts) {
-    io.setShooterVoltage(volts.baseUnitMagnitude());
-  }
-
-  public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
-    return m_sysIdRoutine.quasistatic(direction);
-  }
-
-  public Command sysIdDynamic(SysIdRoutine.Direction direction) {
-    return m_sysIdRoutine.dynamic(direction);
-  }
 }
