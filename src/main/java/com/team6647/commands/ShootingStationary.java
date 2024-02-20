6573ab533e0e7a7e206ed0a5ad66f6f5b7aeb4c6
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-19 19:51:57 -0600

    Created shooting while moving and driving

---
+++ src/main/java/com/team6647/commands/ShootingStationary.java
@@ -0,0 +1,76 @@
+/**
+ * Written by Juan Pablo Gutiérrez 
+ */
+package com.team6647.commands;
+
+import com.andromedalib.andromedaSwerve.subsystems.AndromedaSwerve;
+import com.team6647.subsystems.SuperStructure;
+import com.team6647.subsystems.SuperStructure.SuperStructureState;
+import com.team6647.subsystems.flywheel.ShooterSubsystem;
+import com.team6647.subsystems.flywheel.ShooterSubsystem.FlywheelState;
+import com.team6647.subsystems.shooter.ShooterPivotSubsystem;
+import com.team6647.subsystems.shooter.ShooterRollerSubsystem;
+import com.team6647.subsystems.shooter.ShooterPivotSubsystem.ShooterPivotState;
+import com.team6647.util.Constants.FieldConstants.Speaker;
+import com.team6647.util.Constants.RobotConstants.RollerState;
+import com.team6647.util.ShootingCalculatorUtil;
+import com.team6647.util.ShootingCalculatorUtil.ShootingParameters;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ShootingStationary extends Command {
+
+  private AndromedaSwerve swerve;
+  private ShooterSubsystem flywheelSubsystem;
+  private ShooterPivotSubsystem pivotSubsystem;
+  private ShooterRollerSubsystem rollerSubsystem;
+  private SuperStructure superStructure;
+
+  private ShootingParameters parameters;
+
+  /** Creates a new ShootingStationary. */
+  public ShootingStationary(AndromedaSwerve swerve, ShooterSubsystem flywheelSubsystem,
+      ShooterPivotSubsystem pivotSubsystem, ShooterRollerSubsystem rollerSubsystem, SuperStructure superStructure) {
+    this.swerve = swerve;
+    this.flywheelSubsystem = flywheelSubsystem;
+    this.pivotSubsystem = pivotSubsystem;
+    this.rollerSubsystem = rollerSubsystem;
+
+    addRequirements(swerve, flywheelSubsystem, pivotSubsystem, rollerSubsystem);
+
+    this.superStructure = superStructure;
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.parameters = ShootingCalculatorUtil.getShootingParameters(swerve.getPose(),
+        Speaker.centerSpeakerOpening.toTranslation2d());
+
+    superStructure.updateShootingParameters(parameters);
+
+  }
+
+  @Override
+  public void execute() {
+    swerve.driveSetpoint(parameters.robotAngle());
+    flywheelSubsystem.changeFlywheelState(FlywheelState.STOPPED);
+    pivotSubsystem.setShooterPivotState(ShooterPivotState.SHOOTING);
+
+    if (flywheelSubsystem.topInTolerance() && flywheelSubsystem.bottomInTolerance()) {
+      rollerSubsystem.changeRollerState(RollerState.INTAKING);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/com/team6647/commands/ShootingWhileMoving.java
@@ -0,0 +1,59 @@
+/**
+ * Written by Juan Pablo Gutiérrez
+ */
+package com.team6647.commands;
+
+import static edu.wpi.first.units.Units.Meters;
+
+import com.andromedalib.andromedaSwerve.subsystems.AndromedaSwerve;
+import com.andromedalib.util.AllianceFlipUtil;
+import com.team6647.subsystems.SuperStructure;
+import com.team6647.util.Constants.FieldConstants.Speaker;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class ShootingWhileMoving extends Command {
+
+  private AndromedaSwerve swerve;
+  private SuperStructure superStructure;
+  private Translation2d targetLocation;
+
+  /** Creates a new ShootingWhileMoving. */
+  public ShootingWhileMoving(AndromedaSwerve swerve, SuperStructure superStructure) {
+    this.swerve = swerve;
+    this.superStructure = superStructure;
+    targetLocation = new Translation2d(Meters.of(0.06), Meters.of(5.54));
+
+    targetLocation = AllianceFlipUtil.apply(targetLocation);
+  }
+
+  @Override
+  public void initialize() {
+
+  }
+
+  @Override
+  public void execute() {
+    Pose2d robotPose = swerve.getPose();
+
+    Translation2d speakerLoc = Speaker.centerSpeakerOpening.toTranslation2d();
+    Translation2d robotTranslation = robotPose.getTranslation();
+
+    Translation2d chassisToTarget = speakerLoc.minus(robotTranslation);
+    Rotation2d angle = chassisToTarget.getAngle().rotateBy(Rotation2d.fromDegrees(180));
+
+    swerve.driveSetpoint(angle);
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
