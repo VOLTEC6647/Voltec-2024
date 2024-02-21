4dec857ad3ba7daa3c5eabfdddc549f693f8578c
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-02-21 08:11:27 -0600

    Added vision align to Stationary shooting

--- src/main/java/com/team6647/commands/ShootingStationary.java
+++ src/main/java/com/team6647/commands/ShootingStationary.java
@@ -4,18 +4,24 @@
 package com.team6647.commands;
 
 import com.andromedalib.andromedaSwerve.subsystems.AndromedaSwerve;
+import com.andromedalib.util.AllianceFlipUtil;
+import com.andromedalib.vision.LimelightHelpers;
 import com.team6647.subsystems.SuperStructure;
-import com.team6647.subsystems.SuperStructure.SuperStructureState;
 import com.team6647.subsystems.flywheel.ShooterSubsystem;
 import com.team6647.subsystems.flywheel.ShooterSubsystem.FlywheelState;
 import com.team6647.subsystems.shooter.ShooterPivotSubsystem;
 import com.team6647.subsystems.shooter.ShooterRollerSubsystem;
 import com.team6647.subsystems.shooter.ShooterPivotSubsystem.ShooterPivotState;
+import com.team6647.subsystems.vision.VisionSubsystem;
+import com.team6647.util.Constants.DriveConstants;
+import com.team6647.util.Constants.VisionConstants;
 import com.team6647.util.Constants.FieldConstants.Speaker;
 import com.team6647.util.Constants.RobotConstants.RollerState;
 import com.team6647.util.ShootingCalculatorUtil;
 import com.team6647.util.ShootingCalculatorUtil.ShootingParameters;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 
 public class ShootingStationary extends Command {
@@ -24,22 +30,27 @@ public class ShootingStationary extends Command {
   private ShooterSubsystem flywheelSubsystem;
   private ShooterPivotSubsystem pivotSubsystem;
   private ShooterRollerSubsystem rollerSubsystem;
-  private SuperStructure superStructure;
+  private VisionSubsystem visionSubsystem;
 
   private ShootingParameters parameters;
 
+  private int stageID;
+
   /** Creates a new ShootingStationary. */
   public ShootingStationary(AndromedaSwerve swerve, ShooterSubsystem flywheelSubsystem,
-      ShooterPivotSubsystem pivotSubsystem, ShooterRollerSubsystem rollerSubsystem, SuperStructure superStructure) {
+      ShooterPivotSubsystem pivotSubsystem, ShooterRollerSubsystem rollerSubsystem, VisionSubsystem visionSubsystem) {
     this.swerve = swerve;
     this.flywheelSubsystem = flywheelSubsystem;
     this.pivotSubsystem = pivotSubsystem;
     this.rollerSubsystem = rollerSubsystem;
+    this.visionSubsystem = visionSubsystem;
 
     addRequirements(swerve, flywheelSubsystem, pivotSubsystem, rollerSubsystem);
 
-    this.superStructure = superStructure;
+    stageID = AllianceFlipUtil.shouldFlip() ? VisionConstants.speakerRedCentgerTagID
+        : VisionConstants.speakerBlueCenterTagID;
 
+    //visionSubsystem.changePipeline(VisionConstants.speakerPipelineNumber);
   }
 
   // Called when the command is initially scheduled.
@@ -48,12 +59,36 @@ public class ShootingStationary extends Command {
     this.parameters = ShootingCalculatorUtil.getShootingParameters(swerve.getPose(),
         Speaker.centerSpeakerOpening.toTranslation2d());
 
-    superStructure.updateShootingParameters(parameters);
+    SuperStructure.updateShootingParameters(parameters);
   }
 
   @Override
   public void execute() {
-    swerve.driveSetpoint(parameters.robotAngle());
+    if (visionSubsystem.hasTargetID(stageID)) {
+      // kP (constant of proportionality)
+      // this is a hand-tuned number that determines the aggressiveness of our
+      // proportional control loop
+      // if it is too high, the robot will oscillate around.
+      // if it is too low, the robot will never reach its target
+      // if the robot never turns in the correct direction, kP should be inverted.
+      double kP = .035;
+
+      // tx ranges from (-hfov/2) to (hfov/2) in degrees. If your target is on the
+      // rightmost edge of
+      // your limelight 3 feed, tx should return roughly 31 degrees.
+      double targetingAngularVelocity = visionSubsystem.getTX() * kP;
+
+      // convert to radians per second for our drive method
+      targetingAngularVelocity *= DriveConstants.maxAngularVelocity;
+
+      // invert since tx is positive when the target is to the right of the crosshair
+      targetingAngularVelocity *= -1.0;
+
+      swerve.drive(new Translation2d(), targetingAngularVelocity, true);
+    } else {
+      swerve.driveSetpoint(parameters.robotAngle(), false);
+    }
+
     flywheelSubsystem.changeFlywheelState(FlywheelState.SHOOTING);
     pivotSubsystem.setShooterPivotState(ShooterPivotState.SHOOTING);
 
@@ -65,6 +100,7 @@ public class ShootingStationary extends Command {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    visionSubsystem.changePipeline(VisionConstants.odometryPipelineNumber);
   }
 
   // Returns true when the command should end.
