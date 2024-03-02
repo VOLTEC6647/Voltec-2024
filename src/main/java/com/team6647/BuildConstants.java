c19310d78ac029b7c16797ebdc24fafb17524a8a
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-03-02 09:48:08 -0600

    Update at "02/03/2024, 09:48:08"

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 318;
-  public static final String GIT_SHA = "617767edf1ca1f382320565d391b05ed139b697e";
-  public static final String GIT_DATE = "2024-03-01 22:29:52 GMT-06:00";
+  public static final int GIT_REVISION = 319;
+  public static final String GIT_SHA = "f1bae36095b778e3fb44f28ad5052a2f52004380";
+  public static final String GIT_DATE = "2024-03-02 09:45:39 GMT-06:00";
   public static final String GIT_BRANCH = "event_mxmo";
-  public static final String BUILD_DATE = "2024-03-01 22:31:21 GMT-06:00";
-  public static final long BUILD_UNIX_TIME = 1709353881744L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-03-02 09:45:40 GMT-06:00";
+  public static final long BUILD_UNIX_TIME = 1709394340622L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
--- src/main/java/com/team6647/subsystems/shooter/ShooterCommands.java
+++ src/main/java/com/team6647/subsystems/shooter/ShooterCommands.java
@@ -30,7 +30,7 @@ public class ShooterCommands {
         Debouncer debounce = new Debouncer(0.1, DebounceType.kRising);
 
         return Commands.deadline(
-                Commands.waitUntil(() -> debounce.calculate(!shooterSubsystem.getBeamBrake())),
+                Commands.waitUntil(() -> !shooterSubsystem.getBeamBrake()),
                 new ShooterPivotTarget(pivotSubsystem, ShooterPivotState.INDEXING),
                 new ShooterRollerTarget(rollerSubsystem, RollerState.INTAKING),
                 new FlywheelTarget(shooterSubsystem, FlywheelState.STOPPED));
