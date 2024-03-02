617767edf1ca1f382320565d391b05ed139b697e
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-03-01 22:29:52 -0600

    Build fixes

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 316;
-  public static final String GIT_SHA = "a774256b9741685f936321efa2e95c454c1663ec";
-  public static final String GIT_DATE = "2024-03-01 18:11:00 GMT-06:00";
+  public static final int GIT_REVISION = 317;
+  public static final String GIT_SHA = "3611b30db3039c851b9e2ee9c11c2227192cec07";
+  public static final String GIT_DATE = "2024-03-01 18:12:28 GMT-06:00";
   public static final String GIT_BRANCH = "event_mxmo";
-  public static final String BUILD_DATE = "2024-03-01 18:11:01 GMT-06:00";
-  public static final long BUILD_UNIX_TIME = 1709338261605L;
+  public static final String BUILD_DATE = "2024-03-01 18:12:29 GMT-06:00";
+  public static final long BUILD_UNIX_TIME = 1709338349311L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
