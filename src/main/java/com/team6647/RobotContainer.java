be749f82c982592f58985d2b7224d1bb063a9f31
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-04-05 08:14:03 -0600

    Fixed intake

--- src/main/java/com/team6647/RobotContainer.java
+++ src/main/java/com/team6647/RobotContainer.java
@@ -245,7 +245,9 @@ public class RobotContainer extends SuperRobotContainer {
                                                                 controllerRumbleCommandFactory.apply(0.2),
                                                                 Commands.waitSeconds(0.1),
                                                                 controllerRumbleCommandFactory.apply(0.2)));
-                new Trigger(() -> !shooterSubsystem.getBeamBrake()).whileTrue(new LimelightBlinkEffect(visionSubsytem));
+                new Trigger(() -> !shooterSubsystem.getBeamBrake())
+                                .whileTrue(new InstantCommand(() -> visionSubsytem.setLimelightMode(2)).withTimeout(3)
+                                                .andThen(new InstantCommand(() -> visionSubsytem.setLimelightMode(1))));
         }
 
         SlewRateLimiter xLimiter, yLimiter, turningLimiter;
