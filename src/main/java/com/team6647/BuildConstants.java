9b4eb4dacc156a3805fbc35aabcdae826a248e96
VOLTEC6647 <116227776+SecondaryProgrammer@users.noreply.github.com>
2024-04-08 17:50:18 -0600

    After Oklahoma changes

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -11,8 +11,8 @@ public final class BuildConstants {
   public static final String GIT_SHA = "d78cedb246992584a70c8e0814a3131659b64b5a";
   public static final String GIT_DATE = "2024-04-05 00:01:19 CDT";
   public static final String GIT_BRANCH = "event_okok";
-  public static final String BUILD_DATE = "2024-04-05 13:37:14 CDT";
-  public static final long BUILD_UNIX_TIME = 1712342234510L;
+  public static final String BUILD_DATE = "2024-04-05 16:32:18 CDT";
+  public static final long BUILD_UNIX_TIME = 1712352738510L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
