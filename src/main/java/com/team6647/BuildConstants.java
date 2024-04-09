bb3574518dc6b57f4d1a92c7dac308ba4e9e3caa
AndromedaHelix <58833781+AndromedaHelix@users.noreply.github.com>
2024-04-08 18:02:18 -0600

    Updated Limelight lib

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 288;
-  public static final String GIT_SHA = "30fd0ff5716d6575b484239399e6013b2a1f5237";
-  public static final String GIT_DATE = "2024-04-02 19:23:55 GMT-05:00";
-  public static final String GIT_BRANCH = "dev";
-  public static final String BUILD_DATE = "2024-04-02 20:46:53 GMT-05:00";
-  public static final long BUILD_UNIX_TIME = 1712108813435L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 225;
+  public static final String GIT_SHA = "0d81e1029dcdeb348b3463cf55501242076d118e";
+  public static final String GIT_DATE = "2024-04-08 18:53:26 GMT-05:00";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-04-08 18:57:47 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1712620667730L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
