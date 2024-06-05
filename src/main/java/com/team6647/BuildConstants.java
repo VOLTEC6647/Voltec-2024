cb01beb9c09b709c18ab8ddd78cd82eb48171ecd
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-06-05 16:18:11 -0600

    Updated Libs

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 263;
-  public static final String GIT_SHA = "c0e6d9749bf7a01f05e25b9631a4a53d27bd0b48";
-  public static final String GIT_DATE = "2024-04-19 18:03:42 CDT";
-  public static final String GIT_BRANCH = "event_champs";
-  public static final String BUILD_DATE = "2024-04-20 07:15:16 CDT";
-  public static final long BUILD_UNIX_TIME = 1713615316373L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 226;
+  public static final String GIT_SHA = "4c0bfeb7430a617d5bc57bbd53a5a47e2151a90c";
+  public static final String GIT_DATE = "2024-04-28 23:44:24 GMT-05:00";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2024-06-05 17:17:51 GMT-05:00";
+  public static final long BUILD_UNIX_TIME = 1717625871164L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
