13d33b09f1a02a08286a060e21cfac65ef86e9c8
VOLTEC6647 <116227776+SecondaryProgrammer@users.noreply.github.com>
2024-08-21 18:44:29 -0600

    updated offsets

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 263;
-  public static final String GIT_SHA = "c0e6d9749bf7a01f05e25b9631a4a53d27bd0b48";
-  public static final String GIT_DATE = "2024-04-19 18:03:42 CDT";
+  public static final int GIT_REVISION = 264;
+  public static final String GIT_SHA = "7ea4df196ae9673ec45903e464a36e3e9e17e3b8";
+  public static final String GIT_DATE = "2024-04-23 16:28:31 CDT";
   public static final String GIT_BRANCH = "event_champs";
-  public static final String BUILD_DATE = "2024-04-20 07:15:16 CDT";
-  public static final long BUILD_UNIX_TIME = 1713615316373L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2024-08-21 19:00:43 CDT";
+  public static final long BUILD_UNIX_TIME = 1724284843941L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
