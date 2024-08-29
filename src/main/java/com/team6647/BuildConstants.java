ae8080d31b735520544d0c7c191c8c0e50170336
VOLTEC6647 <116227776+SecondaryProgrammer@users.noreply.github.com>
2024-08-28 20:01:33 -0600

    adignore

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 268;
-  public static final String GIT_SHA = "4695ca73c16d455f4d18aa603890441b28e523f7";
-  public static final String GIT_DATE = "2024-08-28 19:14:26 CDT";
-  public static final String GIT_BRANCH = "nochar";
-  public static final String BUILD_DATE = "2024-08-28 19:51:10 CDT";
-  public static final long BUILD_UNIX_TIME = 1724892670215L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 269;
+  public static final String GIT_SHA = "0e7b40c02bcb268ffa366a0a519074625ed95879";
+  public static final String GIT_DATE = "2024-08-28 20:57:39 CDT";
+  public static final String GIT_BRANCH = "rechar";
+  public static final String BUILD_DATE = "2024-08-28 21:00:56 CDT";
+  public static final long BUILD_UNIX_TIME = 1724896856747L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
