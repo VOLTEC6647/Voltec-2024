97b7333d1da07beacaf3fe8a56add4b9ed74151a
VOLTEC6647 <a01287576@tec.mx>
2024-09-30 15:28:32 -0600

    debug

--- src/main/java/com/team6647/BuildConstants.java
+++ src/main/java/com/team6647/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Voltec-2024";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 292;
-  public static final String GIT_SHA = "9f2b1aaf7d3e89cea0269137fc37f7f3c26f3b48";
-  public static final String GIT_DATE = "2024-09-24 20:00:35 CDT";
+  public static final int GIT_REVISION = 295;
+  public static final String GIT_SHA = "b703348e950e48a50ba109dab94fa063b4f08f29";
+  public static final String GIT_DATE = "2024-09-28 16:13:56 CDT";
   public static final String GIT_BRANCH = "newauto";
-  public static final String BUILD_DATE = "2024-09-26 19:29:40 CDT";
-  public static final long BUILD_UNIX_TIME = 1727396980622L;
+  public static final String BUILD_DATE = "2024-09-28 18:24:02 CDT";
+  public static final long BUILD_UNIX_TIME = 1727565842894L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
